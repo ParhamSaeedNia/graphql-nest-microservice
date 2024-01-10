3f85e0552df07e6f2b7a0032b1eefc288957a10a
ParhamSaeedNia <parhammm76@gmail.com>
2024-01-10 20:43:41 +0330

    post service

--- apps/posts/src/posts.module.ts
+++ apps/posts/src/posts.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PostsResolver } from './posts.resolver';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [PostsController],
-  providers: [PostsService],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  providers: [PostsResolver, PostsService],
 })
 export class PostsModule {}
